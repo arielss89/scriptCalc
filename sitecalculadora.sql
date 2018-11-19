7ac0290442dbc7a8469ea631a4d9d0814e8f461e
arielss89 <42618345+arielss89@users.noreply.github.com>
2018-11-18 21:22:46 -0300

    Update do banco de dados
    Inclusão da senha criptografada

--- sitecalculadora.sql
+++ sitecalculadora.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 15-Nov-2018 às 23:05
+-- Generation Time: 19-Nov-2018 às 01:17
 -- Versão do servidor: 10.1.34-MariaDB
 -- PHP Version: 7.2.8
 
@@ -21,69 +21,88 @@ SET time_zone = "+00:00";
 --
 -- Database: `sitecalculadora`
 --
-CREATE DATABASE IF NOT EXISTS `sitecalculadora` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `sitecalculadora`;
 
 -- --------------------------------------------------------
 
 --
 -- Estrutura da tabela `nutricionista`
 --
--- Criação: 15-Nov-2018 às 20:47
---
 
-CREATE TABLE IF NOT EXISTS `nutricionista` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `nutricionista` (
+  `id` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `senha` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- RELATIONSHIPS FOR TABLE `nutricionista`:
---
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `nutricionista`
 --
 
 INSERT INTO `nutricionista` (`id`, `nome`, `email`, `senha`) VALUES
-(1, 'TesteNutri', 'teste@teste', 'teste');
+(1, 'TesteNutri', 'teste@teste', '698dc19d489c4e4db73e28a713eab07b'),
+(2, 'Filomena', 'filomena@gmail.com', '698dc19d489c4e4db73e28a713eab07b'),
+(4, 'Maria', 'maria@maria', '280f5324cbe33b72ebeefbf2f879e216');
 
 -- --------------------------------------------------------
 
 --
 -- Estrutura da tabela `paciente`
 --
--- Criação: 15-Nov-2018 às 21:09
---
 
-CREATE TABLE IF NOT EXISTS `paciente` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `paciente` (
+  `id` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `idade` int(11) NOT NULL,
   `sexo` int(1) NOT NULL,
   `peso` float NOT NULL,
   `altura` float NOT NULL,
   `ati` int(1) NOT NULL,
-  `id_nutri` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_nutri` (`id_nutri`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- RELATIONSHIPS FOR TABLE `paciente`:
---   `id_nutri`
---       `nutricionista` -> `id`
---
+  `id_nutri` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `paciente`
 --
 
 INSERT INTO `paciente` (`id`, `nome`, `idade`, `sexo`, `peso`, `altura`, `ati`, `id_nutri`) VALUES
-(1, 'Ariel', 29, 0, 65, 1.8, 3, 1);
+(1, 'Ariel', 29, 0, 65, 1.8, 3, 1),
+(2, 'Gabriella', 26, 1, 71, 1.65, 3, 1),
+(3, 'JoÃ£o', 30, 0, 80, 1.9, 5, 1),
+(4, 'Ariel', 55, 0, 80, 1.8, 6, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `nutricionista`
+--
+ALTER TABLE `nutricionista`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `paciente`
+--
+ALTER TABLE `paciente`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_nutri` (`id_nutri`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `nutricionista`
+--
+ALTER TABLE `nutricionista`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `paciente`
+--
+ALTER TABLE `paciente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
